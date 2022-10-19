e96082108968c57b838b6af1585f4117d03061bc
patwyzlic <patrickwyzlic@gmail.com>
2022-10-19 15:07:18 -0400

    Revert "Triangle Class with Area"
    This reverts commit 136279690982a55e9ed013b78a6b1a6d3391c033.

--- triangle/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
-
-        double triangleAArea = triangleA.findArea();
-
-        System.out.println(triangleAArea);
-    }
-}
